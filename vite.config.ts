db0b9aac1bd6476583bb7e02193b5a4ee58693f4
lencx <cxin1314@gmail.com>
2021-09-02 23:57:00 +0800

    chore: error

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,21 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [reactRefresh(), tsconfigPaths()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          lodash: ['lodash'],
+          react: [
+            'react',
+            'react-dom',
+            'react-helmet-async',
+            'react-router-dom',
+            'recoil',
+          ],
+          graphql: ['@apollo/client', 'graphql'],
+        },
+      },
+    },
+  },
 });
